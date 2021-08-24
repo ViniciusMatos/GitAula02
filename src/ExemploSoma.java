57edd07b690030946f0e482d2f37d709bc16afab
ViniciusMatos <vmatos12@gmail.com>
2021-08-24 17:36:37 -0300

    Adicionando variável e operação de divisão

--- src/ExemploSoma.java
+++ src/ExemploSoma.java
@@ -5,10 +5,12 @@ public class ExemploSoma {
 		int num2 = 10;
 		int num3 = 9;
 		int num4 = 5; 
+		int num5 = 2;
 		
 		System.out.println("O resultado da soma é: "+num1+num2);
 		System.out.println("O resutlado da multiplicação é:"+num1*num2);
 		System.out.println("O resultado da subtração é: " +(num2-num4));
+		System.out.println("O resultado da divisão é: " +(num2/num5));
 	}
 
 }
