deedd8b6341beb971d2c699280bf789fad85c5af
ViniciusMatos <vmatos12@gmail.com>
2021-08-24 16:53:57 -0300

    Adicionando nova variável e operação subtração

--- src/ExemploSoma.java
+++ src/ExemploSoma.java
@@ -4,9 +4,11 @@ public class ExemploSoma {
 		int num1 = 8;
 		int num2 = 10;
 		int num3 = 9;
+		int num4 = 5; 
 		
 		System.out.println("O resultado da soma é: "+num1+num2);
 		System.out.println("O resutlado da multiplicação é:"+num1*num2);
+		System.out.println("O resultado da subtração é: " +(num2-num4));
 	}
 
 }
